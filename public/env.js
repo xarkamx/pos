a9c7a4d3e476bded0c0092f355327495d90fc3e5
Alberto <xarkamx@gmail.com>
2023-04-02 15:32:14 -0600

    WIP

--- public/env.js
+++ public/env.js
@@ -1,1 +1,3 @@
-window.env = {};
+window.env = {
+  "REACT_BOS_API_URL": "http://127.0.0.1:8000/"
+};
--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 import env from "react-dotenv";
 
-
+console.log(env, '')
 export const config = {
   apis: {
-    bos: env.REACT_APP_API_BOS,
+    bos: env.REACT_BOS_API_URL,
   }
 }
